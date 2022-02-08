e61f001811789d1fe5a54cc3b989cb4f370b3a44
spark-156 <lucabergman@hotmail.nl>
2022-02-08 09:38:18 +0100

    FIX setup fastapi with debugging

--- backend/main.py
+++ backend/main.py
@@ -6,9 +6,10 @@ app = FastAPI()
 
 @app.get("/{update_id}")
 async def root(update_id: str):
-    print("udpateId:", update_id)
+    print("updateId:", update_id)
     update_id += "this has been updated"
     return {"message": update_id}
 
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("__main__:app", host="0.0.0.0", port=8000, reload=True, workers=1)
